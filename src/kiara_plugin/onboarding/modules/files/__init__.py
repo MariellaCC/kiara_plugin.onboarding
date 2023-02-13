6a35fc62ecc40efcba717c936cc28ef04bbed5e4
Markus Binsteiner <markus@frkl.io>
2023-02-13 12:28:51 +0000

    chore: preparation work for generic import.file module

--- src/kiara_plugin/onboarding/modules/files/__init__.py
+++ src/kiara_plugin/onboarding/modules/files/__init__.py
@@ -6,8 +6,10 @@ import tempfile
 from typing import Any, Dict, Union
 
 from kiara import KiaraModule, KiaraModuleConfig, ValueMap, ValueMapSchema
-from kiara.models.filesystem import FileBundle, FileModel
+from kiara.models.filesystem import FileBundle
 from pydantic import Field
+
+from kiara_plugin.onboarding.utils.download import download_file
 
 
 class DownloadFileConfig(KiaraModuleConfig):
@@ -59,36 +61,11 @@ class DownloadFileModule(KiaraModule):
 
     def process(self, inputs: ValueMap, outputs: ValueMap):
 
-        from datetime import datetime
-
-        import httpx
-        import pytz
-
         url = inputs.get_value_data("url")
         file_name = inputs.get_value_data("file_name")
-        tmp_file = tempfile.NamedTemporaryFile(delete=False)
-        atexit.register(tmp_file.close)
 
-        history = []
-        datetime.utcnow().replace(tzinfo=pytz.utc)
-        with open(tmp_file.name, "wb") as f:
-            with httpx.stream("GET", url, follow_redirects=True) as r:
-                history.append(dict(r.headers))
-                for h in r.history:
-                    history.append(dict(h.headers))
-                for data in r.iter_bytes():
-                    f.write(data)
+        result_file, metadata = download_file(url=url, file_name=file_name)
 
-        if not file_name:
-            # TODO: make this smarter, using content-disposition headers if available
-            file_name = url.split("/")[-1]
-
-        result_file = FileModel.load_file(tmp_file.name, file_name)
-
-        metadata = {
-            "response_headers": history,
-            "request_time": datetime.utcnow().replace(tzinfo=pytz.utc).isoformat(),
-        }
         outputs.set_value("download_metadata", metadata)
         outputs.set_value("file", result_file)
 
