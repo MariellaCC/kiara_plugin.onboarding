6e476616d4e774fcf9b8df6a30618b4b07954eab
Mariella CC <MariellaCC@users.noreply.github.com>
2023-09-26 13:10:20 +0200

    feat: add query status module

--- src/kiara_plugin/onboarding/modules/commoncrawl.py
+++ src/kiara_plugin/onboarding/modules/commoncrawl.py
@@ -12,13 +12,13 @@ from kiara.exceptions import KiaraProcessingException
 from kiara.models.filesystem import KiaraFileBundle
 
 
-class GetCommoncrawlIndexes(KiaraModule):
-    """Get Common Crawl archives indexes for a given query performed via Amazon Web Services (AWS) and Athena.
+class RunCcQuery(KiaraModule):
+    """Execute a Common Crawl archives indexes for a given query performed via Amazon Web Services (AWS) and Athena.
     This process requires an AWS account and an S3 bucket. It may trigger some fees billed by AWS.
     Additional information on the process followed available at: https://commoncrawl.org/2018/03/index-to-warc-files-and-urls-in-columnar-format/.
     """
 
-    _module_type_name = "onboard.get_cc_indexes"
+    _module_type_name = "onboard.run_cc_query"
 
     def create_inputs_schema(
         self,
@@ -38,8 +38,8 @@ class GetCommoncrawlIndexes(KiaraModule):
     ) -> ValueMapSchema:
 
         return {
-            "cc_indexes": {
-                "type": "dict",
+            "cc_query_id": {
+                "type": "string",
             }
         }
 
@@ -113,9 +113,64 @@ class GetCommoncrawlIndexes(KiaraModule):
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/athena/client/get_query_execution.html
         # test if query is still running
 
-        try:
-            response = client.get_query_results(QueryExecutionId=query_id)
-        except Exception as e:
-            print(f"Error returning query results: {e}")
+        # try:
+        #     response = client.get_query_results(QueryExecutionId=query_id)
+        # except Exception as e:
+        #     print(f"Error returning query results: {e}")
         
-        outputs.set_value("cc_indexes", response)
+        outputs.set_value("cc_query_id", query_id)
+
+
+
+
+class GetCcQueryStatus(KiaraModule):
+    """Get the status of a Common Crawl archives indexes query.
+    """
+
+    _module_type_name = "onboard.get_cc_query_status"
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {
+            "cc_query_id": {"type": "string", "doc": "AWS/Athena query id."},
+            "aws_access_key_id": {"type": "string", "doc": "The AWS access key id."},
+            "aws_secret_access_key": {"type": "string", "doc": "The AWS secret access key."},
+        }
+
+    def create_outputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {
+            "cc_query_status": {
+                "type": "dict",
+            }
+        }
+
+
+    def process(self, inputs: ValueMap, outputs: ValueMap):
+
+        import boto3
+
+        query_id = inputs.get_value_data("cc_query_id")
+        aws_access_key_id = inputs.get_value_data("aws_access_key_id")
+        aws_secret_access_key = inputs.get_value_data("aws_secret_access_key")
+
+        # AWS region (must be us-east-1 for commoncrawl usage)
+        region_name = 'us-east-1'
+
+        # set up Athena with AWS credentials
+        try:
+            client = boto3.client('athena', region_name=region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        except Exception as e:
+            print(f"Error setting-up AWS/Athena client: {e}")
+
+        # get query status
+        try:
+            response = client.get_query_execution(QueryExecutionId=query_id)
+        except Exception as e:
+            print(f"Error returning query execution: {e}")
+        
+        outputs.set_value("cc_query_status", response)
